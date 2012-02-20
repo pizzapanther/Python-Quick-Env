b4c51273c2213f48e83f256e4083220392c9c23a
Paul Bailey <paul.m.bailey@gmail.com>
2012-02-20 10:13:50 -0600

    add home command

--- pyquickenv/__main__.py
+++ pyquickenv/__main__.py
@@ -10,13 +10,26 @@ def main ():
   j = fh.read()
   fh.close()
   config = json.loads(j)
+  cmd = None
   
-  if sys.argv[1] in config.keys():
-    env = config[sys.argv[1]]
-    venv = os.path.join(env['env'], 'bin', 'activate')
-    cmd = "source  %s" % venv
-    if env.has_key('cd') and env['cd']:
-      cmd += '; cd ' + env['cd']
+  if os.environ.has_key('VIRTUAL_ENV'):
+    env_path = os.environ['VIRTUAL_ENV']
+    for key, env in config.items():
+      if env['env'] == env_path:
+        if env.has_key('cd') and env['cd']:
+          cmd = 'cd ' + env['cd']
+          
+    if not cmd:
+      cmd = "echo Your in a virtual env, but there is no directory to change to."
       
+  if not cmd and len(sys.argv) == 2:
+    if sys.argv[1] in config.keys():
+      env = config[sys.argv[1]]
+      venv = os.path.join(env['env'], 'bin', 'activate')
+      cmd = "source  %s" % venv
+      if env.has_key('cd') and env['cd']:
+        cmd += '; cd ' + env['cd']
+        
+  if cmd:
     print cmd
     
