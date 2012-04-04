219adcb9a562dd1f4bd787016ff119e51fdaeca1
Paul Bailey <paul.m.bailey@gmail.com>
2012-04-04 17:43:37 -0500

    added add command

--- pyquickenv/__main__.py
+++ pyquickenv/__main__.py
@@ -4,12 +4,26 @@ import os
 import sys
 import json
 
-def main ():
+def get_config ():
   jpath = os.path.join(os.environ['HOME'], '.pyenv.json')
+  if not os.path.exists(jpath):
+    return {}
+    
   fh = open(jpath, 'r')
   j = fh.read()
   fh.close()
   config = json.loads(j)
+  
+  return config
+  
+def save_config (config):
+  jpath = os.path.join(os.environ['HOME'], '.pyenv.json')
+  fh = open(jpath, 'w')
+  j = fh.write(json.dumps(config, indent=2))
+  fh.close()
+  
+def main ():
+  config = get_config()
   cmd = None
   
   if len(sys.argv) == 1 and os.environ.has_key('VIRTUAL_ENV'):
@@ -22,14 +36,29 @@ def main ():
     if not cmd:
       cmd = "echo Your in a virtual env, but there is no directory to change to."
       
-  if not cmd and len(sys.argv) == 2:
-    if sys.argv[1] in config.keys():
-      env = config[sys.argv[1]]
-      venv = os.path.join(env['env'], 'bin', 'activate')
-      cmd = "source  %s" % venv
-      if env.has_key('cd') and env['cd']:
-        cmd += '; cd ' + env['cd']
+  if not cmd:
+    if len(sys.argv) > 1:
+      if sys.argv[1] == 'add' and len(sys.argv) > 2:
+        envdir = os.path.join(os.environ['HOME'], 'pyenv')
+        if not os.path.exists(envdir):
+          os.mkdir(envdir)
+          
+        venvdir = os.path.join(envdir, sys.argv[2])
+        cmd = 'virtualenv "%s"' % venvdir
         
+        config[sys.argv[2]] = {'env': venvdir}
+        if len(sys.argv) > 3:
+          config[sys.argv[2]]['cd'] = sys.argv[3]
+          
+        save_config(config)
+        
+      if sys.argv[1] in config.keys():
+        env = config[sys.argv[1]]
+        venv = os.path.join(env['env'], 'bin', 'activate')
+        cmd = "source  %s" % venv
+        if env.has_key('cd') and env['cd']:
+          cmd += '; cd ' + env['cd']
+          
   if cmd:
     print cmd
     
